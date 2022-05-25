4ccf18e16e2bdf892395586c09ebc604c942475f
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-05-25 22:25:16 +0200

    fix header request

--- tacheParent.php
+++ tacheParent.php
@@ -1,8 +1,25 @@
 <?php
 	include "cnx.php";
 	$request_method = $_SERVER["REQUEST_METHOD"];
-	header("Access-Control-Allow-Origin: *");
-		
+
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+	
+    // Access-Control headers are received during OPTIONS requests
+    if ($request_method == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
 	function getTacheParent($idType) {
 		global $cnx;
 		$sql = $cnx->prepare("select parent.nomType FROM type AS parent LEFT JOIN type AS child ON child.idparent = parent.idType WHERE child.idType = ?");
